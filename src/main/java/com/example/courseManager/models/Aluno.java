728303f2a92554a5bfe13316d11cff7e7fe2bf1f
bernardo <bernardo_nf@hotmail.com>
2025-10-31 21:27:11 -0300

    feat(models): create Aluno and Curso entities

---
+++ src/main/java/com/example/courseManager/models/Aluno.java
@@ -0,0 +1,80 @@
+package com.example.courseManager.models;
+
+import jakarta.persistence.*;
+
+@Entity // Define a classe como uma entidade gerenciável, permitindo ao JPA mapeá-la para uma tabela no H2
+public class Aluno {
+
+    @Id // Garante que o Id será uma chave primária no banco de dados
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera um id automático a partir de uma requisição POST
+    private long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String matricula;
+
+    @ManyToOne
+    @JoinColumn(name = "curso_id") // Definindo o nome da foreign key no banco de dados
+    private Curso curso;
+
+    public Aluno(long id, String nome, String cpf, String email, String matricula, Curso curso) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.matricula = matricula;
+        this.curso = curso;
+    }
+
+    public Aluno() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+}
+
---
+++ src/main/java/com/example/courseManager/models/Curso.java
@@ -0,0 +1,72 @@
+package com.example.courseManager.models;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity // Define a classe como uma entidade gerenciável, permitindo ao JPA mapeá-la para uma tabela no H2
+public class Curso {
+
+    @Id // Garante que o Id será uma chave primária no banco de dados
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Gera um id automático a partir de uma requisição POST
+    private long id;
+    private String nome;
+    private String descricao;
+    private Integer cargaHoraria;
+
+    @OneToMany(mappedBy = "curso")
+    List<Aluno> alunos = new ArrayList<>();
+
+
+    public Curso(long id, String nome, String descricao, Integer cargaHoraria) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public Curso() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+}
+
