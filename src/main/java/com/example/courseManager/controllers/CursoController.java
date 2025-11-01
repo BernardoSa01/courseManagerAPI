a639899a4a7a70866917ed8c3c60ea1952d288ed
bernardo <bernardo_nf@hotmail.com>
2025-11-01 12:08:40 -0300

    feat(controller): implement Curso REST controller

---
+++ src/main/java/com/example/courseManager/controllers/CursoController.java
@@ -0,0 +1,76 @@
+package com.example.courseManager.controllers;
+
+import com.example.courseManager.dtos.CursoRequestDTO;
+import com.example.courseManager.dtos.CursoResponseDTO;
+import com.example.courseManager.mappers.CursoMapper;
+import com.example.courseManager.models.Curso;
+import com.example.courseManager.service.CursoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/cursos")
+public class CursoController {
+
+    @Autowired
+    private CursoService cursoService;
+
+    @Autowired
+    private CursoMapper cursoMapper;
+
+    @PostMapping("/save")
+    public ResponseEntity<CursoResponseDTO> create(@RequestBody CursoRequestDTO dto) {
+
+        Curso curso = cursoMapper.toEntity(dto);
+
+        Curso cursoCriado = cursoService.save(curso);
+
+        CursoResponseDTO response = cursoMapper.toDto(cursoCriado);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CursoResponseDTO> findCursoById(@PathVariable Long id) {
+        Optional<Curso> curso = cursoService.findById(id);
+
+        if (curso.isPresent()) {
+            CursoResponseDTO cursoDto = cursoMapper.toDto(curso.get());
+            return ResponseEntity.status(HttpStatus.OK).body(cursoDto);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CursoResponseDTO>> findAll() {
+        List<Curso> cursos = cursoService.findAll();
+
+        // Lista vazia para receber dtos
+        List<CursoResponseDTO> cursosDto = new ArrayList<>();
+
+        for (Curso curso : cursos) {
+            CursoResponseDTO cursoDto = cursoMapper.toDto(curso);
+            cursosDto.add(cursoDto);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(cursosDto);
+    }
+
+    @PutMapping("/{id}")
+    public Curso updateCurso(@PathVariable Long id, @RequestBody Curso curso) {
+        return cursoService.update(id, curso);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCurso(@PathVariable Long id) {
+        cursoService.deleteById(id);
+    }
+}
+
